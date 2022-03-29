51305b79002f7cbd098d234d7e29a858b65f0320
Squirral Chips <94378581+Kuromishi@users.noreply.github.com>
2022-03-29 14:39:59 +0800

    two control input

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerID
+{
+    _P1,
+    _P2
+}
+
+public class PlayerLogic : MonoBehaviour
+{
+    float horizontalInput;
+    float verticalInput;
+    public float movementSpeed;
+    public float jumpHeight;
+    public float turnSpeed;
+    public float lookSpeed;
+    public Transform cameraContainer;
+    private Rigidbody rigidBody;
+    private Vector3 processedInput;
+    private float processedTurnInput;
+    private float processedLookInput;
+    private bool isGround;
+    private bool canJump;
+    protected Animator anim;
+
+    CharacterController characterController;
+
+    [SerializeField]
+    PlayerID m_playerID;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("Grounded", true);
+    }
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        horizontalInput = Input.GetAxis("Horizontal"+m_playerID);
+        verticalInput = Input.GetAxis("Vertical"+m_playerID);
+        processedInput = transform.right * horizontalInput + transform.forward * verticalInput;
+
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            anim.SetFloat("Speed", movementSpeed);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0);
+        }
+
+        if (Input.GetButtonDown("Jump"+m_playerID) && isGround == true)
+        {
+            canJump = true;
+        }
+        if (isGround == false)
+        {
+            anim.SetBool("Grounded", false);
+        }
+        else
+        {
+            anim.SetBool("Grounded", true);
+        }
+        var turnInput = Input.GetAxis("Mouse X"+m_playerID);
+        processedTurnInput = turnInput;
+        var lookInput = Input.GetAxis("Mouse Y"+m_playerID);
+        processedLookInput = lookInput;
+        cameraContainer.Rotate(new Vector3(processedLookInput, 0f, 0f) * lookSpeed * Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(transform.position + (processedInput * movementSpeed * Time.fixedDeltaTime));
+        rigidBody.MoveRotation(Quaternion.Euler(transform.eulerAngles + (Vector3.up * processedTurnInput) * turnSpeed * Time.deltaTime));
+        if (canJump)
+        {
+            rigidBody.AddForce(new Vector3(0, jumpHeight, 0));
+            canJump = false;
+        }
+    }
+    public void OnCollisionEnter(Collision collision) //Better way£ºRaycast
+    {
+        if (collision.gameObject.GetComponent<ground>() != null)
+        {
+            isGround = true;
+        }
+
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<ground>() != null)
+        {
+            isGround = false;
+        }
+    }
+}

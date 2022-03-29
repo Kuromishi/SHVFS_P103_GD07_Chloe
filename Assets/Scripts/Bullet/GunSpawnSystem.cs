ab4ad5692a4c34d248c7d1aab81d8018a12f6b76
Squirral Chips <94378581+Kuromishi@users.noreply.github.com>
2022-03-29 21:22:17 +0800

    GunPoint Random Spawn

--- Assets/Scripts/Bullet/GunSpawnSystem.cs
+++ Assets/Scripts/Bullet/GunSpawnSystem.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GunSpawnSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject GunSpawnPointPrefabs;
+    public Transform[] GunSpawnPointTransform;
+    public float tickRate;
+    public float tickTimer = 0;
 
-    // Update is called once per frame
-    void Update()
+
+    public void Update()
     {
-        
+        tickTimer += Time.deltaTime;
+        if (tickTimer < tickRate) return;
+        var index= Random.Range(0, GunSpawnPointTransform.Length);
+        var gun= Instantiate(GunSpawnPointPrefabs, GunSpawnPointTransform[index].position, Quaternion.identity);//销毁生成的这个，并不是prefab
+        tickTimer = 0;
+
+        Destroy(gun, 4);
     }
 }
